94f96c749e92fadc8cd0cfd30de4c5e94833201e
Alex <iamkoshkin89@gmail.com>
2024-07-03 13:29:29 +0300

    Conditions practice done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let numberOfFilms;
+const numberOfFilms = +prompt('How many films have you ever watched', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,62 +10,30 @@ const personalMovieDB = {
     privat: false
 };
 
-start();
-rememberMyFilms();
-detectPersonalLevel();
-myFavoriteGenres();
-showMyBD();
+for ( let i = 0; i < 2; i++) {
 
+    const a = prompt('One of the latest wached films', ''),
+          b = prompt(' How can you rate it? ');
 
- function start() {
-    numberOfFilms = +prompt('How many films have you already watched', '');
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms) ) {
-        numberOfFilms = +prompt('How many films have you already watched', '');
-    }
- }
-
- function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('One of the latest watched films', ''),
-              b = prompt('How can you rate it', '');
-    
-        if ( a != null && b != null && a != '' && b != '' && a.length < 50 ) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('Error');
-            i--;
-        }
-    }
- }
-
- function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Too low');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-        console.log('Enough');
-    } else if (personalMovieDB.count >= 30 ) {
-        console.log('Too much');
+    if ( a != null && b != null && a != '' && b != '' && a.length < 50  ) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
     } else {
-        console.log('Error');
-    }
- }
+        console.log('error');
+        i--;
+    }  
+}
 
- function myFavoriteGenres() {
-     for( let i=1; i <= 3; i++ ) {
-        const kindGenres = prompt(`What is your favorite genres ${i}`, '');
-        if( kindGenres != '' && kindGenres != null ) {
-            personalMovieDB.genres[i - 1] = kindGenres;
-        } else {
-            console.log('Genres ERROR');
-            i--;
-        }    
-     }
- }
 
- function showMyBD() {
-     if( personalMovieDB.privat == false ) {
-         console.log(personalMovieDB);
-     }
- }
+if ( personalMovieDB.count < 10 ) {
+    console.log('You haven`t watched enough films');
+} else if ( personalMovieDB.count >= 10 &&  personalMovieDB.count < 30 ) {
+    console.log( 'You are good enough' );
+} else if ( personalMovieDB.count >= 30 ) {
+    console.log( 'You are awesome filmwatcher' );
+} else {
+    console.log( 'Wthere is a mistake' );
+}
 
+console.log(personalMovieDB.count);
+console.log(personalMovieDB);
